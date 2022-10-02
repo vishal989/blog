5127ae87834f8de3b83f17747e9f1fba94f2199a
vishal989 <vavishalarya@gmail.com>
2022-10-02 11:24:01 +0530

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+import Register from './pages/Register';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Single from './pages/Single';
+import Write from './pages/Write';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Home!</div>,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/home',
+    element: <Home />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/single',
+    element: <Single />,
+  },
+  {
+    path: '/write',
+    element: <Write />,
+  },
+]);
+
 function App() {
   return (
     <div>
-      Hello world!
+      <RouterProvider router={router} />
     </div>
   );
 }
