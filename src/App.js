adb072d9c26b0bca5cb4cdf9e4758f88a288e516
vishal989 <vavishalarya@gmail.com>
2022-10-02 15:07:27 +0530

    login form

--- src/App.js
+++ src/App.js
@@ -1,41 +1,64 @@
-import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from 'react-router-dom';
 import Register from './pages/Register';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Single from './pages/Single';
 import Write from './pages/Write';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import "./style.scss";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Home!</div>,
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/post/:id',
+        element: <Single />,
+      },
+      {
+        path: '/write',
+        element: <Write />,
+      },
+    ],
   },
   {
     path: '/register',
     element: <Register />,
-  }, 
-  {
-    path: '/home',
-    element: <Home />,
   },
   {
     path: '/login',
     element: <Login />,
   },
-  {
-    path: '/single',
-    element: <Single />,
-  },
-  {
-    path: '/write',
-    element: <Write />,
-  },
 ]);
 
 function App() {
   return (
-    <div>
-      <RouterProvider router={router} />
+    <div className="app">
+      <div className="container"> 
+        <RouterProvider router={router} />   
+      </div>
+
     </div>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="auth">
+      <h1>Login</h1>
+      <form>
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+        <button>Login</button>
+        <span>
+          Don't you have an account? <Link to="/register">Register</Link>
+        </span>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
