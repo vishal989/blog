a079ae5d434ccb5554be919144ab3df9392dece3
vishal989 <vavishalarya@gmail.com>
2022-10-08 19:05:51 +0530

    Menu component

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import Edit from '../img/edit.png';
+import Delete from '../img/delete.png';
+import { Link } from 'react-router-dom';
+import Menu from '../components/Menu';
 
 const Single = () => {
   return (
-    <div>Single</div>
-  )
-}
+    <div className="single">
+      <div className="content">
+        <img src={Edit} alt="" />
+        <div className="user">
+          <img src={Delete} alt="" />
+          <div className="info">
+            <span>John</span>
+            <p>Posted 2 days ago</p>
+          </div>
+          <div className="edit">
+            <Link to={`/write?edit=2`}>
+              <img src={Edit} alt="" />
+            </Link>
+            <img src={Delete} alt="" />
+          </div>
+        </div>
+        <h1>Lorem!!</h1>
+        <p>Paragraph</p>
+      </div>
+      <Menu/>
+    </div>
+  );
+};
 
-export default Single
+export default Single;
